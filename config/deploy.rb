b9e185917842a69bf68175b12e7b6d5701ed0e43
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-10-14 14:09:58 +0900

    訂正2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.16.0'
 # デプロイするアプリケーション名
-set :application, 'achieve3'
+set :application, 'achieve'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/yo-shi-da/achieve3.git'
